fef893811917ac4648c58c540d23fa517bc0dcaa
SeleneHdz01 <hdz_f@hotmail.com>
2020-01-13 22:39:54 -0600

    fix conect to db

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,6 +1,6 @@
 <?php 
 /**MySQLi connect to DB */
-$conn = mysqli_connect('localhost', 'selene', 'SelHdz_15', 'sel_bd');
+$conn = mysqli_connect('localhost', 'selene', 'SelHdz_15', 'sel_pizza');
 
 /**Check connection */
 if(!$conn){
